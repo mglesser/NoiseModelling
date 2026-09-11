3f7cb5418bb51e07eba5b24f8914eb1770f0b637
Martin Glesser <martin.glesser@univ-eiffel.fr>
2026-09-11 14:52:59 +0200

    feat: spherical wave reflection coefficient

--- noisemodelling-propagation/src/main/java/org/noise_planet/noisemodelling/propagation/harmonoise/HarmonoiseAttenuation.java
+++ noisemodelling-propagation/src/main/java/org/noise_planet/noisemodelling/propagation/harmonoise/HarmonoiseAttenuation.java
@@ -9,9 +9,9 @@
 
 package org.noise_planet.noisemodelling.propagation.harmonoise;
 
+import org.apache.commons.math3.complex.Complex;
 import org.locationtech.jts.algorithm.Angle;
 import org.locationtech.jts.geom.Coordinate;
-import org.noise_planet.noisemodelling.pathfinder.utils.ComplexNumber;
 import org.noise_planet.noisemodelling.propagation.SceneWithAttenuation;
 
 import java.util.Arrays;
@@ -166,25 +166,104 @@ public class HarmonoiseAttenuation {
         return isConvex;
     }
 
-    private ComplexNumber sphericalWaveReflectionCoefficient(ComplexNumber groundImpedance, double angle,
-                                                             double frequency, double distance){
-        ComplexNumber cosAngle = new ComplexNumber(Math.cos(angle),0);
-        ComplexNumber z =  ComplexNumber.multiply(groundImpedance, cosAngle);
-        ComplexNumber planeWaveReflectionCoefficient = ComplexNumber.divide(
-                ComplexNumber.subtract(z, new ComplexNumber(1,0)),
-                ComplexNumber.add(z, new ComplexNumber(1,0))
-        );
-        ComplexNumber kr = new ComplexNumber(2 * Math.PI * frequency / scene.defaultCnossosParameters.getCelerity()
-                * distance, 0);
-        ComplexNumber admittance = ComplexNumber.divide(new ComplexNumber(1, 0), groundImpedance);
-        ComplexNumber numericalDistance = ComplexNumber.multiply(
-            new ComplexNumber(0.5, 0.5),
-            ComplexNumber.multiply(
-                ComplexNumber.pow(kr, 0.5),
-                ComplexNumber.add(cosAngle, admittance)
-            )
-        );
-        ComplexNumber reflectionCoefficient = new ComplexNumber(1,1);
-        return reflectionCoefficient;
+    /**
+     * Compute the spherical wave reflection coefficient
+     * Ref: "Spherical-wave reflection coefficient" subsection of section 2.4.1 from Salomons et al.
+     * Ref: K. Attenborough, K. M. Li, and K. Horoshenkov, Predicting Outdoor Sound. Taylor & Francis, 2006.
+     * doi: 10.1201/9781482295023. (section 2.3)
+     *
+     * @param groundImpedance normalized ground impedance[]
+     * @param angle reflection angle with respect to the normal on the segment
+     * @param frequency frequency [Hz]
+     * @param distance total distance between the image source and the receiver, through the segment
+     * @param hm (hS + hR) / 2
+     * @return spherical wave reflection coefficient
+     */
+    private Complex sphericalWaveReflectionCoefficient(Complex groundImpedance, double angle,
+                                                             double frequency, double distance, double hm){
+        Complex z =  groundImpedance.multiply(Math.cos(angle));
+        Complex planeWaveReflectionCoefficient = z.subtract(1).divide(z.add(1));
+        double kr = 2 * Math.PI * frequency / scene.defaultCnossosParameters.getCelerity() * distance;
+        Complex admittance = groundImpedance.reciprocal();
+        Complex numericalDistance = admittance.multiply(Math.sqrt(kr))
+                .multiply(new Complex(0.5, 0.5));
+        double hG = scene.defaultCnossosParameters.celerity / frequency / 32;
+        double nG = 1 - 0.7 * Math.exp(-hm / hG);
+        return boundaryLossFactor(numericalDistance).pow(nG)
+                .multiply(new Complex(1).subtract(planeWaveReflectionCoefficient))
+                .add(planeWaveReflectionCoefficient);
+    }
+
+    /**
+     * Compute the boundary loss factor F(w)
+     * Ref: K. Attenborough, K. M. Li, and K. Horoshenkov, Predicting Outdoor Sound. Taylor & Francis, 2006.
+     * doi: 10.1201/9781482295023. (section 2.3)
+     *
+     * @param w numerical distance
+     * @return boundary loss factor
+     */
+    private Complex boundaryLossFactor(Complex w){
+        double x = w.getReal();
+        double y = w.getImaginary();
+        // Compute z = exp(-w^2) * erfc(-iw) for different value of the numerical distance
+        Complex z;
+        if ( Math.abs(x) > 3.9 || Math.abs(y) > 3){
+            if ( Math.abs(x) > 6. || Math.abs(y) > 6.) {
+                z = new Complex(0, 1).multiply(w)
+                        .multiply(
+                                w.pow(2).subtract(0.2752551).reciprocal().multiply(0.5124242)
+                                .add(w.pow(2).subtract(2.724745).reciprocal().multiply(0.05176536))
+                        );
+            } else {
+                z = new Complex(0, 1).multiply(w)
+                        .multiply(
+                                w.pow(2).subtract(0.1901635).reciprocal().multiply(0.461313500)
+                                .add(w.pow(2).subtract(1.7844927).reciprocal().multiply(0.099992160))
+                                .add(w.pow(2).subtract(5.5253437).reciprocal().multiply(0.002883894))
+                        );
+            }
+        } else {
+            double h = 0.8;
+            double a1 = Math.cos(2 * x * y);
+            double b1 = Math.sin(2 * x * y);
+            double c1 = Math.exp(-2 * y * Math.PI / h) - Math.cos(2 * x * Math.PI / h);
+            double d1 = Math.sin(2 * x * Math.PI / h);
+            double cd = c1*c1 + d1*d1;
+            double p = 1;
+            double q = 1;
+            if (cd != 0){
+                double expArgument = -(Math.pow(x, 2) + 2 * y * Math.PI / h - Math.pow(y, 2));
+                p = 2 * Math.exp(expArgument) * (a1 * c1 - b1 * d1) / cd;
+                q = 2 * Math.exp(expArgument) * (a1 * d1 - b1 * c1) / cd;
+            }
+            double eh = Math.pow(10, -6);
+            double h1 = 0;
+            double h2 = 0;
+            for (int n=1; n<=5; n++){
+                double x1 = (y*y+x*x+n*n*h*h);
+                double x2 = (y*y-x*x+n*n*h*h);
+                double x3 = (y*y+x*x-n*n*h*h);
+                double denominator = x2 * x2 + 4 * y * y * x * x;
+                double numeratorFactor = Math.exp(-1 * (n * n) * h * h);
+                h1 += numeratorFactor * x1 / denominator;
+                h2 += numeratorFactor * x3 / denominator;
+            }
+            double denominator = Math.PI * (x * x + y * y);
+            double k1 = h*y / denominator + 2*y*h*h1/Math.PI - y*eh/Math.PI;
+            double k2 = h*x / denominator + 2*x*h*h2/Math.PI + x*eh/Math.PI;
+            if (y < Math.PI/h)
+            {
+                k1 += p;
+                k2 -= q;
+            }
+            if (y == Math.PI/h)
+            {
+                k1 += 0.5 * p;
+                k2 -= 0.5 * q;
+            }
+            z = new Complex (k1, k2) ;
+        }
+        // return boundary loss factor
+        return w.multiply(new Complex(0,1).multiply(Math.sqrt(Math.PI))).multiply(z).add(1);
     }
 }
